9fe3863633aa25f32ebcef381503516e16357891
glichys <hayashi@glic.co.jp>
2016-05-26 12:04:52 +0900

    shit

--- Source/PullToRefreshOption.swift
+++ Source/PullToRefreshOption.swift
@@ -20,7 +20,7 @@ public class PullToRefreshOption {
     public var indicatorColor = UIColor.grayColor()
     public var autoStopTime: Double = 0.7 // 0 is not auto stop
     public var fixedSectionHeader = false  // Update the content inset for fixed section headers
-    static let imageName: String = "pulltorefresharrow.png"
+    public var imageName: String = "pulltorefresharrow.png"
     
     public init() {
     }
